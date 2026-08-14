2c6b3fdb82698712b040cabdee972e5957756a91
Taccoa <tacococo2015@outlook.com>
2026-08-14 10:02:04 -0500

    Fixed player collisions

--- player.gd
+++ player.gd
@@ -1,7 +1,15 @@
-extends Area2D
+extends CharacterBody2D
+
+@export var speed: float = 5000.0
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
-func _process(_delta: float) -> void:
-	global_position = get_global_mouse_position()
+func _physics_process(_delta: float) -> void:
+	var target: Vector2 = get_global_mouse_position()
+	var distance: float = global_position.distance_to(target)
+
+	if distance > 1.0:
+		var direction: Vector2 = global_position.direction_to(target)
+		var motion: Vector2 = direction * min(speed * _delta, distance)
+		move_and_collide(motion)
